9a36e5c5498c598a28cf14781ad8d51a18a045a5
bhumi46 <thisisbn46@gmail.com>
2025-07-21 15:10:47 +0530

    updated vpc
    Signed-off-by: bhumi46 <thisisbn46@gmail.com>

--- terraform/modules/aws/outputs.tf
+++ terraform/modules/aws/outputs.tf
@@ -33,3 +33,66 @@ output "K8S_CLUSTER_PRIVATE_IPS_STR" {
 output "K8S_TOKEN" {
   value = module.rke2-setup.K8S_TOKEN
 }
+
+# VPC Information
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = local.vpc_id
+}
+
+output "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+  value       = var.create_vpc ? module.base_infra[0].vpc_cidr_block : data.aws_vpc.existing[0].cidr_block
+}
+
+# Subnet Information
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = local.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = local.private_subnet_ids
+}
+
+# NAT Gateway Information (only when VPC is created)
+output "nat_gateway_ids" {
+  description = "IDs of the NAT Gateways"
+  value       = var.create_vpc ? module.base_infra[0].nat_gateway_ids : []
+}
+
+output "nat_gateway_public_ips" {
+  description = "Public IPs of the NAT Gateways"
+  value       = var.create_vpc ? module.base_infra[0].nat_gateway_public_ips : []
+}
+
+# Security Group
+# Infrastructure Status
+output "vpc_created" {
+  description = "Whether VPC was created in this run"
+  value       = var.create_vpc
+}
+
+# Subnet Details for Reference
+output "subnet_details" {
+  description = "Detailed subnet information for reference"
+  value = var.create_vpc ? {
+    public_subnets = [
+      for i, subnet in module.base_infra[0].public_subnet_ids : {
+        id   = subnet
+        cidr = module.base_infra[0].public_subnet_cidrs[i]
+        az   = var.availability_zones[i]
+        type = "Public"
+      }
+    ]
+    private_subnets = [
+      for i, subnet in module.base_infra[0].private_subnet_ids : {
+        id   = subnet
+        cidr = module.base_infra[0].private_subnet_cidrs[i]
+        az   = var.availability_zones[i]
+        type = "Private"
+      }
+    ]
+  } : null
+}
